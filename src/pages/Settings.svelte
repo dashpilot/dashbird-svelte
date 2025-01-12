a8d3e5614890fbd1747f1274d9e7a5cba949c5ab
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2025-01-12 01:55:51 +0100

    Added settings screen

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -1,4 +1,11 @@
 <script>
     import Nav from '../lib/Nav.svelte';
     let { data = $bindable() } = $props();
+
+    const baseClasses = 'w-full rounded-lg border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-white px-4 py-2 mt-0 mb-5 resize-none text-gray-900';
 </script>
+
+{#each data.settings as item}
+    <label for="status" class="block text-sm font-medium text-gray-700 mb-1 capitalize">{item.key.replaceAll('_', ' ')}</label>
+    <input type="text" class={baseClasses} bind:value={item.value} />
+{/each}
